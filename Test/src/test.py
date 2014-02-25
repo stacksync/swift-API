d7d2fc25c0dd484eb34de971a76dae021d6a26dc
ASTedgar <cesk002@gmail.com>
2014-02-25 11:15:07 +0100

    modified:   Test/src/test.py

--- Test/src/test.py
+++ Test/src/test.py
@@ -19,7 +19,7 @@ headers_param = {'StackSync-API':'v2', 'X-Auth-Token':auth_token}
 
 
 
-class Test(unittest.TestCase):
+class Test_FILE(unittest.TestCase):
     def setUp(self):
         unittest.TestCase.setUp(self)
         self.values = {'name':'Docuemnts'}
@@ -31,22 +31,27 @@ class Test(unittest.TestCase):
     Files Resource
     '''
 
-    def test_put_file(self):
+    def test_put_without_parameters(self):
         '''**** PUT ****'''
         # Without parameters. So no update anything
         req = requests.put(common_url + 'file/123', headers=headers_param)
         assert req.json()['ERROR']
+    def test_put_with_name(self):
         # with name parameter
         req = requests.put(common_url + 'file/123', data=self.values, headers=headers_param)
         assert req.status_code == 200
+    def test_put_check_keys(self):
+        req = requests.put(common_url + 'file/123', data=self.values, headers=headers_param)
         # check dict keys
         content = req.json()
         for k in self.keys:
             assert content.has_key(k), k
-    
+    def test_put_with_name_parent(self):
         # with name and parent parameters
         req = requests.put(common_url + 'file/123', data=self.values_parent, headers=headers_param)
         assert req.status_code == 200
+        
+    def test_put_without_file(self):
         # without file_id
         req = requests.put(common_url + 'file', self.values, headers=headers_param)
         assert req.status_code == 400
@@ -55,16 +60,20 @@ class Test(unittest.TestCase):
         # with file_id
         req = requests.get(common_url + 'file/123', headers=headers_param)
         assert req.status_code == 200
+        
+    def test_get_check_keys(self):   
+        req = requests.get(common_url + 'file/123', headers=headers_param)
         content = req.json()
         # check dict keys
         for k in self.keys:
             assert content.has_key(k), k
+    def test_get_without_fileid(self):
         # without file_id
         req = requests.get(common_url + 'file', headers=headers_param)
         assert req.status_code == 400
         
         # TODO: Test to pass a file_id which not exists. 
-    def test_post_file(self):       
+    def test_post_with_name(self):       
         '''**** POST ****'''
         # with name parameter
         req = requests.post(common_url + 'file', self.values, headers=headers_param)
@@ -72,10 +81,16 @@ class Test(unittest.TestCase):
         content = req.json()
         for k in self.post_keys:
             assert content.has_key(k)
-        
+    def test_post_check_keys(self):
+        req = requests.post(common_url + 'file', self.values, headers=headers_param)
+        content = req.json()
+        for k in self.post_keys:
+            assert content.has_key(k)
+    def test_post_with_name_parent(self):     
         # with parent and name parameters
         req = requests.post(common_url + 'file', self.values_parent, headers=headers_param)
         assert req.status_code == 200
+    def test_post_without_parameters(self):
         # without parameters
         req = requests.post(common_url + 'file', headers=headers_param)
         assert req.status_code == 400
@@ -85,125 +100,169 @@ class Test(unittest.TestCase):
         # with file_id 
         req = requests.delete(common_url + 'file/123', headers=headers_param)
         assert req.status_code == 200
+        
+    def test_delete_check_keys(self):
+        req = requests.delete(common_url + 'file/123', headers=headers_param)
         content = req.json()
-    
         for k in self.keys:
             assert content.has_key(k), k
-            
+    def test_delete_without_fileid(self):        
         # without file_id
         req = requests.delete(common_url + 'file', headers=headers_param)
         assert req.status_code == 400
+        
+        
+class Test_FOLDER(unittest.TestCase):
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+        self.values = {'name':'Docuemnts'}
+        self.values_parent = {'name':'Docuemnts', 'parent':'Cristian Cots'}
+        self.keys = ('name', 'path', 'id', 'mimetype', 'status', 'version', 'parent', 'user', 'client_modified', 'server_modified')
+
+        self.post_keys = ('name', 'path', 'id', 'parent', 'user')
     '''
     Folder Resource
     '''
     
-    def test_post_folder(self):
+    def test_post_without_parameters(self):
         '''**** POST ****'''
         # without parameters
         req = requests.post(common_url + 'folder', headers=headers_param)
         assert req.status_code == 400
-        
+    def test_post_with_name(self):  
         # with name parameter
         req = requests.post(common_url + 'folder', self.values, headers=headers_param)
         assert req.status_code == 200
-        
+    def test_post_check_keys(self):
+        req = requests.post(common_url + 'folder', self.values, headers=headers_param)
         # Check keys of dict
         content = req.json()
         for k in self.post_keys:
             assert content.has_key(k), k
-        
+    def test_post_with_name_parent(self):    
         # with parent and name parameters
         req = requests.post(common_url + 'folder', self.values_parent, headers=headers_param)
         assert req.status_code == 200
         
-    def test_delete_folder(self):
+    def test_delete_with_folderid(self):
         '''**** DELETE ****'''
-    
         # with file_id 
         req = requests.delete(common_url + 'folder/123', headers=headers_param)
         assert req.status_code == 200
+    def test_delete_check_keys(self):
+        req = requests.delete(common_url + 'folder/123', headers=headers_param)
         content = req.json()
         for k in self.keys:
             assert content.has_key(k), k 
+    def test_delete_without_folderid(self):
         # without file_id
         req = requests.delete(common_url + 'folder', headers=headers_param)
         assert req.status_code == 400
-    def test_get_folder(self):   
+        
+    def test_get_with_folderid(self):   
         '''**** GET ****'''
         
         # Get folder metadata. With folder_id
         req = requests.get(common_url + 'folder/123', headers=headers_param)
         assert req.status_code == 200
+    def test_get_check_keys(self):
+        req = requests.get(common_url + 'folder/123', headers=headers_param)
         content = req.json()
         # check dict keys
         for k in self.keys:
             assert content.has_key(k), k
+    def test_get_without_folderid(self):
         # Get folder metadata. Without folder_id
         req = requests.get(common_url + 'folder', headers=headers_param)
         assert req.status_code == 400
         # TODO: Test to pass a folder_id which not exists. NOT FOUND 404
         
-    def test_put_folder(self):   
-    
+    def test_put_without_parameters(self):   
         '''**** PUT ****'''
-        
         # Without parameters. So no update anything
         req = requests.put(common_url + 'folder/123', headers=headers_param)
         assert req.json()['ERROR']
+    def test_put_with_name(self):
         # with name parameter
         req = requests.put(common_url + 'folder/123', data=self.values, headers=headers_param)
-        print req
         assert req.status_code == 200
+    def test_put_check_keys(self):
+        req = requests.put(common_url + 'folder/123', data=self.values, headers=headers_param)
         # check dict keys
         content = req.json()
         for k in self.keys:
             assert content.has_key(k), k
-    
+    def test_put_with_name_parent(self):
         # with name and parent parameters
         req = requests.put(common_url + 'folder/123', data=self.values_parent, headers=headers_param)
         assert req.status_code == 200
+    def test_put_without_folderid(self):
         # without file_id
         req = requests.put(common_url + 'folder', self.values, headers=headers_param)
         assert req.status_code == 400
-        
+    
+class Test_VERSIONS(unittest.TestCase):    
     '''
     Versions Resource
     '''
-    def test_get_versions(self):
-    
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+        self.values = {'name':'Docuemnts'}
+        self.values_parent = {'name':'Docuemnts', 'parent':'Cristian Cots'}
+        self.keys = ('name', 'path', 'id', 'mimetype', 'status', 'version', 'parent', 'user', 'client_modified', 'server_modified')
+
+        self.post_keys = ('name', 'path', 'id', 'parent', 'user')
+        
+    def test_get_specific_versions(self):    
         '''**** GET ****'''
         # with a specific version
         req = requests.get(common_url + 'folder/123/versions/123', headers=headers_param)
         assert req.status_code == 200
+    def test_get_check_keys_specific_version(self):
+        req = requests.get(common_url + 'folder/123/versions/123', headers=headers_param)
         content = req.json()
         try:
             for k in self.keys:
                 assert content.has_key(k), k
         except:
             assert False, "It's mandatory return a dict, so only one version"
+    def test_get_without_specific_version(self):
         # without specific version
         req = requests.get(common_url + 'folder/123/versions', headers=headers_param)
-        content = req.json()
         assert req.status_code == 200
+    def test_get_check_keys_without_specific_version(self):
+        req = requests.get(common_url + 'folder/123/versions', headers=headers_param)
+        content = req.json()
         for dict in content:
             for k in self.keys:
                 assert dict.has_key(k), k    
         # wrong rute
+        
+class Test_CONTENTS(unittest.TestCase):    
     '''
-    Contents Resource
+    Versions Resource
     '''
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+        self.values = {'name':'Docuemnts'}
+        self.values_parent = {'name':'Docuemnts', 'parent':'Cristian Cots'}
+        
+        self.keys = ('name', 'path', 'id', 'status', 'version', 'parent', 'user', 'client_modified', 'server_modified', 'contents')
+        self.contents_key = ('name', 'path', 'id', 'status', 'version', 'parent', 'user', 'client_modified', 'server_modified')         
+
     # GET
     def test_get_contents(self):
-        keys = ('name', 'path', 'id', 'status', 'version', 'parent', 'user', 'client_modified', 'server_modified', 'contents')
-        contents_key = ('name', 'path', 'id', 'status', 'version', 'parent', 'user', 'client_modified', 'server_modified')
-    
         '''**** GET ****'''
         # with a specific version
         req = requests.get(common_url + 'folder/123/contents', headers=headers_param)
         assert req.status_code == 200
+    def test_get_check_keys(self):
+        req = requests.get(common_url + 'folder/123/contents', headers=headers_param)
+
         content = req.json()
+
         # without specific version
-        for k in keys:
+        for k in self.keys:
             assert content.has_key(k), k
             if k == 'contents':
                 for content_dict in content[k]:
@@ -211,24 +270,37 @@ class Test(unittest.TestCase):
                         # Empty folder
                         assert True
                         break
-                    for key in contents_key:
+                    for key in self.contents_key:
                         assert content_dict.has_key(key), key
     '''
     Data Resource
     '''
-    
-    def test_put_data(self):
+class Test_DATA(unittest.TestCase):    
+    '''
+    Versions Resource
+    '''
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+        self.values = {'name':'Docuemnts'}
+        self.values_parent = {'name':'Docuemnts', 'parent':'Cristian Cots'}
+        self.keys = ('name', 'path', 'id', 'mimetype', 'status', 'version', 'parent', 'user', 'client_modified', 'server_modified')
+
+        self.post_keys = ('name', 'path', 'id', 'parent', 'user')   
+    def test_put_with_file_content(self):
         '''**** PUT ****'''
         file_content = {"content":"There are the content of file"}
     
         req = requests.put(common_url + 'file/123/data', data=file_content, headers=headers_param)
         assert req.status_code == 200, str(req.status_code) + req.text
+    def test_put_without_file_content(self):
         req = requests.put(common_url + 'file/123/data', headers=headers_param)
         assert req.status_code == 400, req.status_code
-    def test_get_data(self):    
+        
+    def test_get_file_data(self):    
         '''**** GET ****'''
         req = requests.get(common_url + 'file/123/data', headers=headers_param)
         assert req.status_code == 200, req.status_code
+    def test_get_file_version_data(self):
         req = requests.get(common_url + 'file/123/versions/132/data', headers=headers_param)
         assert req.status_code == 200, req.status_code
       
