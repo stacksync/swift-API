570f33b601c2d375708d37e3be7d5e8e5b3be773
Adrian Moreno <adrian.moreno@urv.cat>
2014-06-04 10:28:00 +0200

    project restructuring

--- twisted/server_twisted/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-StackSync API WEB module for OpenStack Swift
-"""
-
-__all__ = ['version_info', 'version']
-#: Version information ``(major, minor, revision)``.
-version_info = (1, 0, 0)
-#: Version string ``'major.minor.revision'``.
-version = '.'.join(map(str, version_info))
